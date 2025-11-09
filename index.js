272f1236cea16e7025626d9868f1d56d83f81304
AMaupin <alexandrafink8989@gmail.com>
2025-11-09 10:42:08 -0700

    installed readline-sync package, added goal notes

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var rs = require('readline-sync');
 
+//Goals for Project are as follows:
+// Prompt for users name
+// Prompts for users favorite hobby
+// Ask user if they have any more hobbies
+// Store a list of the users hobbies
+// Print all hobbies from list at the end
+
+
+// Prompt for users name
 var userName = rs.question('Hi there, what is your name?');
 console.log('Hello, ' + userName + '. It is very nice to meet you!');
