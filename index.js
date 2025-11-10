1a85a9d1ffa861c3e341abe262ca0401d6d7b0ca
AMaupin <alexandrafink8989@gmail.com>
2025-11-10 08:07:41 -0700

    code clean up and tested, all responses prompted correctly other than a couple spacing issues.

--- index.js
+++ index.js
@@ -15,23 +15,42 @@ function getHobbyCount() {
   moreHobbies = rs.questionInt('How many other hobbies do you have? ')
 };
 
+function addHobbies(numHobbies) {
+  var counter = 0;
+  while (counter < numHobbies) {
+    var input = rs.prompt();
+    hobbies.push(input);
+    counter++;
+
+    if (counter < numHobbies) {
+      console.log('Hey! ' + hobbies + ' is pretty cool! What\'s the other hobby? ')
+    } else {
+      console.log('I wish I could ' + hobbies + '... alas, I am not even real!')
+    }
+  }
+}
+
 // Prompt for users name
 var userName = rs.question('Hi there, what is your name? ');
 console.log('Hello, ' + userName + '. It is very nice to meet you!');
 // Prompts for users favorite hobby
 var favHobby = rs.question(userName + ', what is your favorite hobby? ')
 
+hobbies.push(favHobby);
+getHobbyCount();
+
 // Ask question of other hobbies and push response to hobbies
 if (moreHobbies > 0) {
-  var counter = 0;
-  while (counter < moreHobbies) {
-    var input = rs.prompt();
-    hobbies.push(input);
-    counter++;
-  if (counter < moreHobbies) {
-    console.log('--That is awesome! ' + input + ' is super cool! What is the other ' + (moreHobbies - counter) + '? ');
-  } else {
-    console.log('I wish I could ' + hobbies + '... but alas, I am not real.')
-  }
-  }
-};
+  console.log('Cool, what other hobbies do you like? ');
+  addHobbies(moreHobbies);
+} else {
+  areYouSure = rs.keyInYN('Are you sure that ' + hobbies[0] + ' is everything you enjoy doing? ')
+}
+
+if (!areYouSure && hobbies.length <= 1) {
+  getHobbyCount();
+  console.log('Cool, what are they?');
+  addHobbies(moreHobbies);
+} else if (areYouSure) {
+  console.log(hobbies + ' are all very cool, good for you! ');
+}
